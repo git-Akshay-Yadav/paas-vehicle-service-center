2a7fea903a2514dd60a8f717b649bdd04d4fd096
git-Akshay-Yadav <Aky11Yadav@gmail.com>
2019-01-03 02:51:50 +0530

    Added spring-boot boiler plate

---
+++ src/main/java/per/saas/bsp/bikeServiceApp/DemoApplication.java
@@ -0,0 +1,13 @@
+package per.saas.bsp.bikeServiceApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
